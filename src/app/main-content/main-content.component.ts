c6eba47cf441b6ea774cc716855dbba5b4b49bac
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-05-12 17:33:06 +0200

    finished above the fold and start with about me section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, AboveTheFoldComponent],
+  imports: [
+    HeaderComponent,
+    AboveTheFoldComponent,
+    AboutMeComponent
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
