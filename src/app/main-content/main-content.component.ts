6cb7278013599a181a466efcb84a8e086649f5a3
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-05-13 20:04:27 +0200

    finished the about me and my skills section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-main-content',
@@ -9,7 +10,8 @@ import { AboutMeComponent } from './about-me/about-me.component';
   imports: [
     HeaderComponent,
     AboveTheFoldComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    MySkillsComponent
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
