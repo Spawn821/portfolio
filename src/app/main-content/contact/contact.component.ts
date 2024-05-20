1f405066e4279732b51585b651a06d820a5b4cbc
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-05-20 21:21:02 +0200

    revise the code and images from the diffrent components and it is possible so send emails

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  mailTest = false;
+
+  post = {
+    endPoint: 'https://stephan-schmidt-web-developer.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text'
+      }
+    }
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.sendPost(ngForm);
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log(this.contactData);
+      ngForm.resetForm();
+    }
+  }
+
+  sendPost(ngForm: NgForm) {
+    this.http.post(this.post.endPoint, this.post.body(this.contactData))
+      .subscribe({
+        next: (response) => {
+          console.log(this.contactData);
+          ngForm.resetForm();
+        },
+        error: (error) => {
+          console.error(error);
+        },
+        complete: () => console.info('Send post complete')
+      });
+  }
 }
