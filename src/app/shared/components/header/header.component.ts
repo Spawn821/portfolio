784f2a8ba044625448e6d0751c50a4dedc0bb41e
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-05-22 19:26:31 +0200

    implement favicon, highlighting the nav bar if this was clicked, create privacy policy and imprint

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  lastHighlightedElement: any = {
+    firstElement: undefined,
+    lastElement: undefined
+  };
+
+  isClicked(event: any) {
+    document.addEventListener('wheel', () => {
+      this.removeHighlighting();
+    });
+
+    if (this.lastHighlightedElement.firstElement) {
+      this.removeHighlighting();
+    }
+
+    this.lastHighlightedElement.firstElement = event.target;
+    this.lastHighlightedElement.lastElement = event.target.nextElementSibling;
+
+    event.target.classList.add('highlightingText');
+    event.target.nextElementSibling.classList.remove('v-hidden');
+  }
+
+  removeHighlighting() {
+      this.lastHighlightedElement.firstElement.classList.remove('highlightingText');
+      this.lastHighlightedElement.lastElement.classList.add('v-hidden');
+  }
 }
