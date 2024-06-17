4430f8470316344024a26d7797e2c01bf842d2cf
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-06-17 20:27:27 +0200

    incorporate feedback, use the hole site but the content max is 1440px.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import {AboveTheFoldComponent} from "./main-content/above-the-fold/above-the-fold.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    FooterComponent
-  ],
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        FooterComponent,
+        AboveTheFoldComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
