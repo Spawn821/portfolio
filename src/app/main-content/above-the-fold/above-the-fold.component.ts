e0fda1e61ae6e628041770dfb47a4392d8c890bd
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-06-18 20:39:16 +0200

    dynamic vector banner.

--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -9,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AboveTheFoldComponent {
 
+  constructor() {
+    window.addEventListener('resize', () => {
+      this.positioningVectorBanner();
+    })
+  }
+
+
+  ngOnInit(): void {
+    this.positioningVectorBanner();
+  }
+
+
+  positioningVectorBanner() {
+    let vectorBanner  = document.getElementById('vector-banner');
+    let windowWidth = window.innerWidth;
+
+    let windowWidthConstant = 2500;
+    let translateYConstant = 85;
+
+    let translateY = (windowWidthConstant - windowWidth) * 0.25;
+
+    if (vectorBanner) {
+      vectorBanner.style.top = (translateYConstant + translateY) + 'px';
+    }
+  }
+
 }
