53e01dac27ceffa7389640a76fac618f8b643d11
Stephan Schmidt <81927898+Spawn821@users.noreply.github.com>
2024-07-01 20:37:38 +0200

    make above the fold section resizeable

--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -12,36 +12,52 @@ export class AboveTheFoldComponent {
 
   @Input() languageList: any;
   @Input() currentLanguage: string = '';
+  picturePos: any;
 
   constructor() {
     window.addEventListener('resize', () => {
+      this.pictureRect();
       this.positioningVectorBanner();
     })
+
+    window.onresize = () => {
+      this.pictureRect();
+      this.positioningVectorBanner();
+    }
   }
 
 
   ngOnInit(): void {
+    this.pictureRect();
     this.positioningVectorBanner();
     animationScroll('hidden-left', 'show-left');
     animationScroll('hidden-right', 'show-right');
   }
 
 
+  ngOnChange(): void {
+    this.pictureRect();
+    this.positioningVectorBanner();
+  }
+
+
   positioningVectorBanner() {
     let vectorBanner  = document.getElementById('vector-banner');
-    let windowWidth = window.innerWidth;
-    let windowWidth1440px = window.matchMedia('(max-width: 1440px)');
-    let windowWidth1000px = window.matchMedia('(max-width: 1000px)');
-    let windowWidth550px = window.matchMedia('(max-width: 550px)');
-    let windowHeight1440px = window.matchMedia('(min-height: 1440px)');
 
-    let windowWidthConstant = 2500;
-    let translateYConstant = windowHeight1440px.matches ? 550 : 200;
+    let windowWidth = window.innerWidth;
+    //let windowWidth1440px = window.matchMedia('(max-width: 1440px)');
+
+    let windowWidthConstant = 1500;
+    let translateYConstant = -500;
 
     let translateY = (windowWidthConstant - windowWidth) * 0.25;
 
     if (vectorBanner) {
-      vectorBanner.style.top = (translateYConstant + translateY) + 'px';
+      vectorBanner.style.top = (this.picturePos.bottom + translateYConstant + translateY) + 'px';
     }
+  }
+
+  pictureRect() {
+    this.picturePos = document.querySelector('#profile-picture')?.getBoundingClientRect();
   }
 }
